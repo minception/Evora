927075d988d037e12896ff3c0eb7e28411ea5262
Michal Počatko <mpocatko@gmail.com>
2019-12-13 23:55:38 +0100

    Added Engine project

---
+++ Engine/Engine/Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hi, I am an engine" << endl;
+	return 0;
+}
