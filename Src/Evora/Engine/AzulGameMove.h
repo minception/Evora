355b9f4bf5926210ebf58b1faf3347753081c00f
minception <mpocatko@gmail.com>
2020-05-07 15:36:49 +0200

    Fixed syntax issues for classes AzulGameMove and AzulGameState

--- Src/Evora/Engine/AzulGameMove.h
+++ Src/Evora/Engine/AzulGameMove.h
@@ -2,7 +2,6 @@
 #include "GameMove.h"
 #include "command.h"
 
-using namespace mcts;
 
 class AzulGameMove : public GameMove
 {
@@ -13,7 +12,6 @@ public:
 	friend bool operator== (const AzulGameMove& lhs, const AzulGameMove& rhs);
 	std::shared_ptr<control::command> generateCommand();
 private:
-	virtual ostream& ToString(ostream& ostr) const override;
 	int mMove;
 	int mFactoryId;
 	int mPatternLine;
--- Src/Evora/Engine/AzulGameState.h
+++ Src/Evora/Engine/AzulGameState.h
@@ -3,33 +3,30 @@
 #include <random>
 #include "game_controller.h"
 
-using namespace mcts;
 
 class AzulGameState :
 	public GameState
 {
 public:
 	AzulGameState(control::game_controller state, int player);
-	shared_ptr<GameState> Clone() const override;
-	vector<shared_ptr<const GameMove>> GetMoves() const override;
+	std::shared_ptr<GameState> Clone() const override;
+	std::vector<std::shared_ptr<const GameMove>> GetMoves() const override;
 	void DoMove(const GameMove& move) override;
 	double GetResult(int player) const override;
 	int GetWinner() const override;
 	const GameMove& GetSimulationMove() const override;
 	int GetPlayerWhoJustMoved() const override;
 	bool IsTerminal() const override;
-	shared_ptr<const GameMove> ParseMove(const string& move) const override;
+	std::shared_ptr<const GameMove> ParseMove(const std::string& move) const override;
 private:
-	ostream& ToString(ostream& ostr) const override;
-	// const static int directions[4][2];
 	int mPlayerWhoJustMoved;
 	control::game_controller mState;
 	int mWinner;
 	bool mNotWin;
-	vector<shared_ptr<const GameMove>> mMoves;
+	std::vector<std::shared_ptr<const GameMove>> mMoves;
 	void addMove(int factory_index, int pattern_line_index, int color);
 	void CalculateMoves();
 	bool CheckWinDirections(int player, int x, int y);
 	int Near(int player, int x, int y, int dx, int dy);
-	shared_ptr<mt19937> randomEng;
+	std::shared_ptr<std::mt19937> randomEng;
 };
